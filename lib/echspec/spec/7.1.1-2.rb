57f6213c9377473f25df4d1281483aaf52a340e2
thekuwayama <thekuwayama@gmail.com>
2024-01-27 05:13:39 +0900

    add skelton; 7.1.1-2.rb

---
+++ lib/echspec/spec/7.1.1-2.rb
@@ -0,0 +1,16 @@
+module EchSpec
+  module Spec
+    class Spec7_1_1_2
+      class << self
+        # If the client-facing server accepted ECH, it checks the second
+        # ClientHelloOuter also contains the "encrypted_client_hello"
+        # extension. If not, it MUST abort the handshake with a
+        # "missing_extension" alert. Otherwise, it checks that
+        # ECHClientHello.cipher_suite and ECHClientHello.config_id are
+        # unchanged, and that ECHClientHello.enc is empty. If not, it MUST
+        # abort the handshake with an "illegal_parameter" alert.
+        #
+        # https://datatracker.ietf.org/doc/html/draft-ietf-tls-esni-17#section-7.1.1-2
+    end
+  end
+end
