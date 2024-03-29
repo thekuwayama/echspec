8c203cd7af3794f7620706f160f22bb3b8eeab54
thekuwayama <thekuwayama@gmail.com>
2024-03-29 19:56:28 +0900

    feat: validate_2nd_ch_unchanged_ech

--- lib/echspec/spec/7.1.1-2.rb
+++ lib/echspec/spec/7.1.1-2.rb
@@ -24,7 +24,10 @@ module EchSpec
         #
         # @return [Array of EchSpec::Ok | Err]
         def run(hostname, port, ech_config)
-          [validate_2nd_ch_missing_ech(hostname, port, ech_config)]
+          [
+            validate_2nd_ch_missing_ech(hostname, port, ech_config),
+            validate_2nd_ch_unchanged_ech(hostname, port, ech_config)
+          ]
         end
 
         # @param hostname [String]
@@ -46,7 +49,26 @@ module EchSpec
           Err.new("#{hostname}:#{port} connection refused")
         end
 
-        def send_2nd_ch_missing_ech(socket, hostname, ech_config)
+        # @param hostname [String]
+        # @param port [Integer]
+        # @param ech_config [ECHConfig]
+        #
+        # @return [EchSpec::Ok | Err]
+        def validate_2nd_ch_unchanged_ech(hostname, port, ech_config)
+          socket = TCPSocket.new(hostname, port)
+          recv = send_2nd_ch_unchanged_ech(socket, hostname, ech_config)
+          socket.close
+          return Err.new('did not send expected alert: illegal_parameter') \
+            unless Spec.expect_alert(recv, :illegal_parameter)
+
+          Ok.new(nil)
+        rescue Timeout::Error
+          Err.new("#{hostname}:#{port} connection timeout")
+        rescue Errno::ECONNREFUSED
+          Err.new("#{hostname}:#{port} connection refused")
+        end
+
+        def recv_hrr(socket, hostname, ech_config)
           # send 1st ClientHello
           conn = TLS13Client::Connection.new(socket, :client)
           inner_ech = TTTLS13::Message::Extension::ECHClientHello.new_inner
@@ -80,11 +102,42 @@ module EchSpec
           raise 'not received HelloRetryRequest' \
             unless recv.hrr?
 
+          [conn, ch, recv]
+        end
+
+        def send_2nd_ch_missing_ech(socket, hostname, ech_config)
+          conn, ch1, hrr = recv_hrr(socket, hostname, ech_config)
           # send 2nd ClientHello without ech
-          ch1 = ch
-          hrr = recv
           new_exs = TLS13Client.gen_new_ch_extensions(ch1, hrr)
           new_exs.delete(TTTLS13::Message::ExtensionType::ENCRYPTED_CLIENT_HELLO)
+          ch = TTTLS13::Message::ClientHello.new(
+            legacy_version: ch1.legacy_version,
+            random: ch1.random,
+            legacy_session_id: ch1.legacy_session_id,
+            cipher_suites: ch1.cipher_suites,
+            legacy_compression_methods: ch1.legacy_compression_methods,
+            extensions: new_exs
+          )
+          conn.send_record(
+            TTTLS13::Message::Record.new(
+              type: TTTLS13::Message::ContentType::HANDSHAKE,
+              messages: [ch],
+              cipher: TTTLS13::Cryptograph::Passer.new
+            )
+          )
+
+          recv, = conn.recv_message(TTTLS13::Cryptograph::Passer.new)
+          recv, = conn.recv_message(TTTLS13::Cryptograph::Passer.new) \
+            if recv.is_a?(TTTLS13::Message::ChangeCipherSpec)
+          recv
+        end
+
+        def send_2nd_ch_unchanged_ech(socket, hostname, ech_config)
+          conn, ch1, hrr = recv_hrr(socket, hostname, ech_config)
+          # send 2nd ClientHello with unchanged ech
+          new_exs = TLS13Client.gen_new_ch_extensions(ch1, hrr)
+          new_exs[TTTLS13::Message::ExtensionType::ENCRYPTED_CLIENT_HELLO] =
+            ch1.extensions[TTTLS13::Message::ExtensionType::ENCRYPTED_CLIENT_HELLO]
           ch = TTTLS13::Message::ClientHello.new(
             legacy_version: ch1.legacy_version,
             random: ch1.random,
