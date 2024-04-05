fbb2955169360dd9b52b9bf8c9d690513ba499b7
thekuwayama <thekuwayama@gmail.com>
2024-04-05 20:22:01 +0900

    add Log::MessageStack

--- lib/echspec/spec/7-2.3.1.rb
+++ lib/echspec/spec/7-2.3.1.rb
@@ -129,6 +129,7 @@ module EchSpec
             TTTLS13::Message::ExtensionType::ENCRYPTED_CLIENT_HELLO => inner_ech
           )
         )
+        puts Log::MessageStack.new(inner).marshal # FIXME: printf debug
 
         # offer_ech
         selector = proc { |x| TLS13Client.select_ech_hpke_cipher_suite(x) }
